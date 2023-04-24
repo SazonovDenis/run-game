07e152e25fa82ed5b8b3bcad0073ba15e2da37ab
dvsa <sazonov.den@gmail.com>
2023-04-25 02:12:30 +0600

    -

--- run-game/test/run/game/dao/backstage/TaskCreator_Test.groovy
+++ run-game/test/run/game/dao/backstage/TaskCreator_Test.groovy
@@ -1,5 +1,6 @@
 package run.game.dao.backstage
 
+
 import jandcode.core.apx.test.*
 import jandcode.core.dbm.std.*
 import org.junit.jupiter.api.*
@@ -25,13 +26,12 @@ class TaskCreator_Test extends Apx_Test {
         TaskCreator taskCreator = mdb.create(TaskCreatorImpl)
 
         //
-        for (long idTask = 1001; idTask < 1100; idTask = idTask + 3) {
+        for (long idTask = 1001; idTask < 1200; idTask = idTask + 3) {
             try {
                 DataBox task = taskCreator.loadTask(idTask)
 
                 //
-                println()
-                printTask(task)
+                printTaskOneLine(task)
             } catch (Exception e) {
                 println(e.message)
             }
@@ -39,7 +39,7 @@ class TaskCreator_Test extends Apx_Test {
     }
 
     @Test
-    void createTask() {
+    void createTask1() {
         String word = "different"
         long idItem = mdb.loadQueryRecord("select * from Item where Item.value = :value", [value: word]).getLong("id")
 
@@ -80,11 +80,24 @@ class TaskCreator_Test extends Apx_Test {
         TaskCreator taskCreator = mdb.create(TaskCreatorImpl)
 
         //
-        for (long idItem = 1001; idItem < 1010; idItem = idItem + 3) {
-            for (int i = 0; i < 10; i++) {
-                DataBox task = taskCreator.createTask(idItem, "word-spelling", "word-translate")
-                taskCreator.saveTask(task)
+        long idItem = 1001
+        for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    //
+                    DataBox task = taskCreator.createTask(idItem, "word-spelling", "word-translate")
+
+                    //
+                    taskCreator.saveTask(task)
+
+                    //
+                    printTaskOneLine(task)
+                } catch (Exception e) {
+                    println(e.message)
+                }
             }
+            //
+            idItem = idItem + 7
         }
     }
 
@@ -92,6 +105,10 @@ class TaskCreator_Test extends Apx_Test {
         mdb.resolveDicts(task)
         utils.outTable(task.get("task"))
         utils.outTable(task.get("taskOption"))
+    }
+
+    void printTaskOneLine(DataBox task) {
+        println(task.get("task").getValue("value") + ": " + task.get("taskOption").getUniqueValues("value").join(" | "))
     }
 
 }

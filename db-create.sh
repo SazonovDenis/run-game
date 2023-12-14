5dc9526b684ab8912f1acc847fdf56ceccd4b397
dvsa <dvsa>
2023-12-14 20:24:22 +0600

    -

--- db-create.sh
+++ db-create.sh
@@ -1,4 +1,21 @@
-jc db-create
+# выход при любой ошибке
+set -e
 
-jc db-loadtestdata -s:base
+
+# Сначала соберём приложение
+jc build
+
+
+# Структура базы
+./app-run.sh db-create -drop
+
+
+# Тестовые данные
+./app-run.sh db-loadtestdata -s base
+
+
+# Кубы
+./app-run.sh cube-db-init
+./app-run.sh cube-interval -all -dbeg "2022-01-01" -dend "2023-12-31"
+./app-run.sh cube-audit -all
 

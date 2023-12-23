e640f3f9b2b99d245b134c2e48db471b7c38b10d
dvsa <dvsa>
2023-12-24 01:33:31 +0600

    -

--- run-game/src/run/game/dao/auth/AuthAction.java
+++ run-game/src/run/game/dao/auth/AuthAction.java
@@ -19,14 +19,17 @@ public class AuthAction extends BaseAction {
     public void login() throws Exception {
         AuthService authSvc = getApp().bean(AuthService.class);
         ActionRequestUtils requestUtils = getReq();
+
         //
         String login = requestUtils.getParams().getString("login");
         String password = requestUtils.getParams().getString("password");
-        //
+
+        // Проверим пользователя
         AuthUser authUser = authSvc.login(new DefaultUserPasswdAuthToken(login, password));
-        //
+        // Записываем пользователя в сессию
         requestUtils.getSession().put(AuthConsts.SESSION_KEY_USER, authUser);
-        //
+
+        // Читаем и возвращаем данные пользователя
         String s = UtJson.toJson(authUser.getAttrs());
         requestUtils.render(s);
     }
@@ -37,9 +40,11 @@ public class AuthAction extends BaseAction {
     public void getUserInfo() throws Exception {
         AuthService authSvc = getApp().bean(AuthService.class);
         ActionRequestUtils requestUtils = getReq();
-        //
+
+        // Читаем пользователя из сессии
         AuthUser authUser = authSvc.getCurrentUser();
-        //
+
+        // Читаем и возвращаем данные пользователя
         String s = UtJson.toJson(authUser.getAttrs());
         requestUtils.render(s);
     }
@@ -50,10 +55,14 @@ public class AuthAction extends BaseAction {
     public void logout() throws Exception {
         AuthService authSvc = getApp().bean(AuthService.class);
         ActionRequestUtils requestUtils = getReq();
-        //
+
+        // Убираем пользователя из сессии
         requestUtils.getSession().put(AuthConsts.SESSION_KEY_USER, null);
+
+        // Возвращаем в HTTP заголовках сигнал об окончании сессии
         requestUtils.getHttpRequest().getSession().invalidate();
-        //
+
+        // Возвращаем пустые данные
         String s = UtJson.toJson(new HashMap());
         requestUtils.render(s);
     }

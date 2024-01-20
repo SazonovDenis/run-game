e86e28cdbd6c4321894074a125fa78fe61d9e598
dvsa <dvsa>
2024-01-20 23:40:33 +0600

    Статистика по фактам (вопрос+ответ) - куб, бэкэнд

---
+++ kis-molap-ntbd/test/kis/molap/ntbd/model/cubes/Cube_UsrFactStatistic_Test.groovy
@@ -0,0 +1,13 @@
+package kis.molap.ntbd.model.cubes
+
+import kis.molap.ntbd.model.base.CubeBase_Test
+
+class Cube_UsrFactStatistic_Test extends CubeBase_Test {
+
+    void setUp() throws Exception {
+        cubeName = "Cube_UsrFactStatistic"
+        //
+        super.setUp()
+    }
+
+}

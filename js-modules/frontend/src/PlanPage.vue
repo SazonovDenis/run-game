da74a03d225063722a11c2a2ea50fa1bab047abe
dvsa <dvsa>
2024-05-21 18:41:25 +0500

    -

--- js-modules/frontend/src/PlanPage.vue
+++ js-modules/frontend/src/PlanPage.vue
@@ -321,7 +321,7 @@ export default {
             await gameplay.gameStart(this.planId)
 
             apx.showFrame({
-                frame: '/game', props: {}
+                frame: '/game'
             })
         },
 

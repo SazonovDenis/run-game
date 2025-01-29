cfea968e53cd066c0ba16db728067021e23b4940
dvsa <dvsa>
2025-01-29 15:47:50 +0500

    Более надежный запуск сервиса

--- data/product/app-run-serve.sh
+++ data/product/app-run-serve.sh
@@ -7,4 +7,4 @@ date +"%Y-%m-%d %T" >> /home/wordstrike.me/log/log.txt
 
 cd /home/wordstrike.me/product/ 
 
-/home/wordstrike.me/product/app-run.sh serve -log -p 8080 -c / &
+/home/wordstrike.me/product/app-run.sh serve -log -p 8080 -c /
